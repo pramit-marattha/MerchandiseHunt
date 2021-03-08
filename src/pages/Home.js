37a4f3fcb149fbe622248004fa036cbed7a3aed4
pramit-marattha <pramitmarattha5497@gmail.com>
2021-03-08 09:48:11 +0545

    product listing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,16 @@ import React from "react";
 import { IonPage, IonContent } from "@ionic/react";
 import SmallHeader from "../components/Header/SmallHeader";
 import LargeHeader from "../components/Header/LargeHeader";
+import ProductList from "../components/Product/ProductList";
 
-const Home = () => {
+const Home = (props) => {
   return (
     <IonPage>
       <SmallHeader title="Product Hunt" />
-      <IonContent fullscreen>
+      <IonContent color="medium" fullscreen>
         <LargeHeader title="ProductHunt" />
+        <br></br>
+        <ProductList location={props.location}></ProductList>
       </IonContent>
     </IonPage>
   );
--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -2,13 +2,16 @@ import React from "react";
 import { IonPage, IonContent } from "@ionic/react";
 import SmallHeader from "../components/Header/SmallHeader";
 import LargeHeader from "../components/Header/LargeHeader";
+import ProductList from "../components/Product/ProductList";
 
-const Trending = () => {
+const Trending = (props) => {
   return (
     <IonPage>
       <SmallHeader title="Trending" />
       <IonContent fullscreen>
         <LargeHeader title="Trending" />
+        <br></br>
+        <ProductList location={props.location}></ProductList>
       </IonContent>
     </IonPage>
   );
